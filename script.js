d07acc358e3b8fbd24d421d064aa41c4ea678668
sparvasterino <>
2021-04-12 11:15:44 +0000

    update

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
+const siti = [];
+
 const aggiungiSito = function (event) {
 
   event.preventDefault();
   const url = $("#formUrl").val();
   const title = $("#formTitle").val();
   const category = $("#formCategory").val();
-  const aElem = $("<a>").attr("href", url).text(title);
-  const liElem = $("<li>").append(aElem);
-  $("#lista").append(liElem);
+
   console.log("URL: " + url);
   console.log("Titolo: " + title);
   console.log("Categoria:" + category);
   console.log("--------------");
 
+  const nuovoSito = {
+    url: url,
+    title: title,
+    category: category
+  };
+
+  siti.push(nuovoSito);
+  console.log(siti);
+  scriviListaSito(siti);
+}
+
+const scriviListaSito = function (sito) {
+  const aElem = $("<a>").attr("href", sito[sito.length - 1].url).text(sito[sito.length - 1].title);
+  const liElem = $("<li>").append(aElem);
+  $("#lista").append(liElem);
 }
 
 const init = function () {
 
-  $("#formSubmit").on("click", aggiungiSito);
+  $("form").submit(aggiungiSito);
 
 
 }
